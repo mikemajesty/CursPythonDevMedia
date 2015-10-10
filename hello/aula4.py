eaf079fdf31bea89d43d8950ceba9a1e04edcafd
mikemajesty <mikemajesty2011@gmail.com>
2015-10-09 21:21:08 -0300

    Aula 4

---
+++ hello/aula4.py
@@ -0,0 +1,10 @@
+text_string = "Mike Lima\nis\n"+"a great\n\tcompany";
+print(text_string);
+
+myint = 12345;
+myfloat = 3.14159;
+mystr = "mike";
+
+print("int: ",myint);
+print("Decimal int %d is a integer number" % myint);
+print("hexadeciaml integer %x is integer" % myint);
