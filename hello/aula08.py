822d20cb254eee2111343f6c05a60d3321dba3f9
mikemajesty <mikemajesty2011@gmail.com>
2015-12-04 23:04:45 -0200

    Aula 08 - Estrutura de repetição FOR

---
+++ hello/aula08.py
@@ -0,0 +1,12 @@
+print("Usando Lacos FOR");
+sum = 0;
+
+for number in range(10):
+    sum = sum + number;
+
+print("My number is: ",sum);
+
+for number in range(50,100):
+    if number == 88:
+        break;
+    print(number);
