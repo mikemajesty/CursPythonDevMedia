a619d54daf37cbe9fafc7239cc9a99e034c2022b
mikemajesty <mikemajesty2011@gmail.com>
2015-12-06 21:32:27 -0200

    Aula 09 - Operadores Lógicos

---
+++ hello/aula09.py
@@ -0,0 +1,22 @@
+print("Operadores logicos [IGUAL]");
+if 1 == 1 and 2 == 2:
+    print("Sao iguais");
+else:
+    print("Nao e igual");
+
+if 1 == 1 and 2 == 3:
+    print("Condicao e verdaderia");
+else:
+    print("Condicao nao e verdaderia");
+print("Operadores logicos igual [OU]");
+if 1 == 1 or 2 == 3:
+    print("Condicao e verdaderia");
+else:
+    print("Condicao nao e verdaderia");
+    print("Operadores logicos igual [NOT]");
+if not 1 == 1 or 2 == 2:
+     print("Condicao e verdaderia");
+else:
+    print("Condicao nao e verdaderia");
+
+
