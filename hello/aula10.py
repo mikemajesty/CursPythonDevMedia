d6780c9bba3b97dd6ac14fb5a2531d048cbed2ea
mikemajesty <mikemajesty2011@gmail.com>
2015-12-07 10:13:23 -0200

    Aula 10 -  Funções

---
+++ hello/aula10.py
@@ -0,0 +1,11 @@
+print("Functions");
+
+def sum(arg1,arg2):
+    total = arg1 + arg2;
+    print("This value is: ", total);
+total_sum_1 = sum(100,155);
+total_sum_2 = sum(7,1000);
+print(total_sum_1);
+print(total_sum_1);
+
+print(sum(1,1));
