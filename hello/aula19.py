63a053cc6573e1bf72399f01d68c4a32778b0067
mikemajesty <mikemajesty2011@gmail.com>
2015-12-28 11:17:21 -0200

    Introdução ao CGI com Python

---
+++ hello/aula19.py
@@ -0,0 +1,14 @@
+print('Content-Type:text/html')
+print()
+print("<html>")
+print("<head>")
+print("<title>")
+print("Hello Word");
+print("</title>")
+print("</head>")
+print("<body>");
+print("<h1>Oi Tudo bem, como vc esta!!</h1>")
+print("</body>");
+print("</html>")
+
+
