5f8eeff093a0a1493f39aadcbd4bb6e8e6da97f1
mikemajesty <mikemajesty2011@gmail.com>
2016-01-02 14:59:19 -0200

    Aula 23 - CGI com Python: Arquivos: Salvar e ler em arquivos txt

---
+++ hello/aula23-Ler.py
@@ -0,0 +1,49 @@
+﻿import cgi
+
+
+def printHeader():
+    print("Content-Type:text/html")
+    print()
+    print("<html>")
+    print("<head>")
+    print("<title>")
+    print("Salvando no .txt")
+    print("</title>")
+    print("</head>")
+    print("<body>")
+
+
+def printFooter():   
+    print("</body>")
+    print("</html>")
+printHeader()
+print("<h1>Ler no txt</h1>")#Uma mensagem no form
+print("<form method='Post' action='aula23-Ler.py' enctype='multipart/form-data'>")#Abertura do form
+print("<p>Arquivo:<input type='file' name='file'/></p>")
+print("<hr>")
+print("<p><input type='submit' value='Enviar'/> </p>")#button
+print("</form>")#Fechamento do form
+
+
+form = cgi.FieldStorage()
+
+if "file" in form:
+    fileItem = form["file"]
+    if not fileItem.file:
+        print("<h3>Nenhum arquivo encontrado</h3>")
+    else:
+        txt_file = open(fileItem.filename,"r")
+        print("<hr><h3>Result: </h3>")
+        for line in txt_file:
+            datas = line.split(".")
+            print("<p>Nome: " + datas[0] + "<br>")
+            print("Sobrenome: " + datas[1] + "<br>")
+            print("Sexo: " + datas[2] + "<br>");
+            print("Compania: " + datas[3] + "<br>");
+            print("Cor: " + datas[4] + "<br>");
+            print("</p>");
+else:
+    print("<h3>Nenhum arquivo selecionado</h3>")
+printFooter();
+
+
